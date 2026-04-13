1cabe9f1c2c17a0bde743612c64d9e4962b3b5a7
coll-ins <mainacollins184@gmail.com>
2026-04-13 13:29:11 +0300

    add

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function combineUsers(...args) {
   for(const array of args) {
     combineObject.users = [...combineObject.users, ...array]
   }
+  combineObject.merge_date = new Date().toLocaleDateString('en-US')
+
+  return combineObject
 }
 
 
